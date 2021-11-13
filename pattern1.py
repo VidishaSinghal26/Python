8d618552200e1c5dce71ec69ecf9429c00d1a0ff
VidishaSinghal26 <90644367+VidishaSinghal26@users.noreply.github.com>
2021-11-13 16:58:52 +0530

    Update pattern1.py

--- pattern1.py
+++ pattern1.py
@@ -1,5 +1,9 @@
-n=int(input("Enter no. of lines"))
-for i in range(1,n+1):
-    for j in range(i):
-      print("*",end='')
-    print()  
+Ques - *
+       **
+       ***
+       ****
+Sol- n=int(input("Enter no. of lines"))
+     for i in range(1,n+1):
+        for j in range(i):
+           print("*",end='')
+       print()  
